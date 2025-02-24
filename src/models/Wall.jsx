0df679b6ef3832e523252aedb56c65cfbd2a2eb1
Brico97442 <amanvilledamien@hotmail.fr>
2025-02-24 23:45:00 +0400

    edit wall height

--- src/models/Wall.jsx
+++ src/models/Wall.jsx
@@ -6,7 +6,7 @@ const Wall = () => {
     const entranceGLB = useGLTF("/3d-models/gltf/entrance/wall.glb");
     console.log(entranceGLB)
     return (
-        <mesh position={[0,1,0]} rotation={[0,0,0]}>
+        <mesh position={[0,0.8,0]} rotation={[0,0,0]}>
             <primitive object={entranceGLB.scene}/>
         </mesh>
     );
