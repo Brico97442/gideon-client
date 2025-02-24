1a266836f1c242b4d6aaa99ca970b4812668a77f
Brico97442 <amanvilledamien@hotmail.fr>
2025-02-24 15:32:03 +0400

    edit mobile version

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -64,7 +64,7 @@ function Scene() {
       <div className="main">
         <div className="fixed h-full w-full">
           <div className="absolute top-0 backdrop-blur-[6px] flex justify-center items-center w-full h-full z-50">
-            <div className={`${applicationStart ? 'lg:hidden hidden':'hidden lg:flex'}  flex-col items-center h-full justify-center relative`}>
+            <div className={`${applicationStart ? 'hidden':'flex'} flex-col items-center h-full justify-center relative`}>
               <h1 className="text-white tracking-[0.5em] font-bold uppercase text-2xl lg:text-[72px] w-full box-border">Gideon </h1>
               <div className="flex flex-col items-center absolute bottom-[20px] lg:bottom-[161px]">
                 <h2 className="text-xl text-white whitespace-nowrap">Lancer l'application</h2>
