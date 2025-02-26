bde0f17b47f4127b119cd42702f3a6a46e3eff56
Brico97442 <amanvilledamien@hotmail.fr>
2025-02-26 09:41:36 +0400

    Ajout des fichiers

--- src/models/Pointer.jsx
+++ src/models/Pointer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
-import { useGLTF, useFrame } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber"; // Assurez-vous de l'importer ici
 
 const Pointer = () => {
     const PointerGLB = useGLTF("/3d-models/gltf/cimetarylayout/pointer.glb");
@@ -7,13 +8,11 @@ const Pointer = () => {
 
     // Utiliser useFrame pour faire tourner l'élément en continu sur l'axe Z
     
-        useFrame(() => {
-            if (pointerRef.current) {
-                pointerRef.current.rotation.z += 0.01; // Augmenter la valeur pour ajuster la vitesse de rotation
-            }
-        });
-    
-    
+        // useFrame(() => {
+        //     if (pointerRef.current) {
+        //         pointerRef.current.rotation.y += 0.01; // Augmenter la valeur pour ajuster la vitesse de rotation
+        //     }
+        // });
 
     return (
         <mesh ref={pointerRef} position={[0, 0, 0]} rotation={[0, 0, 0]}>
