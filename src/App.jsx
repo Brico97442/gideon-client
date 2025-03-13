137513699c4a2522e60f687f60beaffe637c8bdf
Brico97442 <amanvilledamien@hotmail.fr>
2025-03-13 12:04:10 +0400

    test borne 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
+// import "./App.css";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { ROUTES } from "./config/routes";
+// import { TombProvider } from "./context/TombContext";
+
+// export default function App() {
+
+//   const router = createBrowserRouter(ROUTES);
+
+//   return (
+//     <TombProvider>
+//       <div>
+//         <RouterProvider router={router}/>
+//       </div>
+//     </TombProvider>
+//   );
+// }
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ROUTES } from "./config/routes";
-import { TombProvider } from "./context/TombContext";
+import { OrbitControls, Stats } from "@react-three/drei";
 
-export default function App() {
-
-  const router = createBrowserRouter(ROUTES);
-
+function App() {
   return (
-    <TombProvider>
-      <div>
-        <RouterProvider router={router}/>
+    <>
+      <div id="canvas" className="w-full h-full">
+        <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+          <Stats />
+          <OrbitControls />
+        </Canvas>
       </div>
-    </TombProvider>
+    </>
   );
 }
+
+export default App;
