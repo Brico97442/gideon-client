e045353cab96024b75eb701b37629709401974ea
WillsProd <164310569+WillsProd@users.noreply.github.com>
2025-03-13 13:19:24 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,9 @@
 import { Canvas, useThree } from "@react-three/fiber";
 import "./App.css";
 import { OrbitControls, Stats } from "@react-three/drei";
+import { useEffect } from "react";
+import { useRef } from "react";
+import * as THREE from 'three';
 
 function App() {
 
@@ -26,13 +29,35 @@ function App() {
     return <OrbitControls onChange={() => invalidate()} />;
   }
 
+  function Instances({ count = 100000, temp = new THREE.Object3D() }) {
+    const instancedMeshRef = useRef()
+    useEffect(() => {
+      // Set positions
+      for (let i = 0; i < count; i++) {
+        temp.position.set(Math.random(), Math.random(), Math.random())
+        temp.updateMatrix()
+        instancedMeshRef.current.setMatrixAt(i, temp.matrix)
+      }
+      // Update the instance
+      instancedMeshRef.current.instanceMatrix.needsUpdate = true
+    }, [])
+    return (
+      <instancedMesh ref={instancedMeshRef} args={[null, null, count]}>
+        <boxGeometry />
+        <meshPhongMaterial />
+      </instancedMesh>
+    )
+  }
+
+
   return (
     <>
       <div id="canvas" className="w-full h-full" >
         <Canvas frameloop="demand">
           <ambientLight intensity={0.1} />
           <directionalLight color="red" position={[0, 0, 5]} />
-          {Array.from({ length: 1000 }).map((_, i) => (
+          <Instances />
+          {/* {Array.from({ length: 1000 }).map((_, i) => (
             <mesh 
               key={i}
               position={[
@@ -50,7 +75,7 @@ function App() {
               <boxGeometry />
               <meshStandardMaterial color={`hsl(${Math.random() * 360}, 50%, 50%)`} />
             </mesh>
-          ))}
+          ))} */}
           <Stats />
           <CameraControls />
         </Canvas>
