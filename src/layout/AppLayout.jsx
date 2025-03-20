f91d391eea075dcc571707d9856c740ce4b1b823
Brico97442 <amanvilledamien@hotmail.fr>
2025-03-20 11:17:45 +0400

    edit height application on mobile

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -3,7 +3,7 @@ import Scene from "../components/Scene";
 
 function AppLayout() {
     return (
-        <main className="fixed left-0 top-0 min-w-screen h-screen w-full">
+        <main className="fixed left-0 top-0 min-w-screen h-full w-full">
             <Scene />
         </main>
     );
